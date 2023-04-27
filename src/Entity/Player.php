0c9f9ebf5ed801c69d9e8d0e6dbd7f1a152fbc50
Evrald <131721929+MuyangoSamEvrald@users.noreply.github.com>
2023-04-27 14:52:52 +0200

    Add files via upload

---
+++ src/Entity/Player.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlayerRepository::class)]
+class Player
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private $id;
+    private $name;
+    private $surname;
+    private $team;    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getName(): ?string
+{
+    return $this->name;
+}
+
+public function setName(string $name): self
+{
+    $this->name = $name;
+
+    return $this;
+}
+
+public function getSurname(): ?string
+{
+    return $this->surname;
+}
+public function setSurname(string $surname): self
+{
+    $this->surname = $surname;
+
+    return $this;
+}
+
+public function getTeam(): ?Team
+{
+    return $this->team;
+}
+public function setTeam(?Team $team): self
+{
+    $this->team = $team;
+
+    return $this;
+}
+}
